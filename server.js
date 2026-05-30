156de3ecd03fd4fbee85b362562cef4a41a627fb
CrayzeH <vvsdfvvs@gmail.com>
2026-05-30 04:14:39 +0300

    serv

--- server.js
+++ server.js
@@ -2439,6 +2439,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.get('/health', (req, res) => {
+    res.status(200).json({ ok: true });
+});
+
 app.get('/index.html', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
